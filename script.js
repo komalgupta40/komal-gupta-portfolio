50484a7b6ce7be94cb51277cb8e84f8ae627a3d1
KOMAL GUPTA <106368264+komalgupta40@users.noreply.github.com>
2026-04-14 12:53:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const toggleBtn = document.getElementById("themeToggle");
 
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+});
+
+// Form
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thanks! I'll get back to you soon.");
+});
